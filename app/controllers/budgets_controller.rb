dfe705cae903f5c8f2c658dcc8c73cd9b5a06601
miyaosummer <yoshihikon9@gmail.com>
2020-07-01 19:51:27 +0900

    資産登録機能の追加

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,2 +1,43 @@
 class BudgetsController < ApplicationController
+  def new
+    @user = User.find(current_user.id)
+    @budget = Budget.new
+    @junres = Junre.all
+  end
+
+  def show
+    @budget = Budget.find(params[:id])
+  end
+
+  def create
+    Budget.create(budgets_parameter)
+    redirect_to root_path
+  end
+
+  def destroy
+    @budget = Budget.find(params[:id])
+    @budget.destroy
+    redirect_to root_path, notice:"削除しました"
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+    @budget = Budget.find(params[:id])
+    @junres = Junre.all
+  end
+
+  def update
+    @budget = Budget.find(params[:id])
+    if @budget.update(budgets_parameter)
+      redirect_to root_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def budgets_parameter
+    params.require(:budget).permit(:name, :money,).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :incomes
     resources :configs
     resources :assets
+    resources :budgets
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tops#index"
